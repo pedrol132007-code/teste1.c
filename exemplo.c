3d427adb8df0b634ad510d39587a5fc702c5b4eb
PedroSantos <pedrol132007@gmail.com>
2026-08-18 04:06:58 +0000

    Algoritmos e ProgramaçãoLab1

--- exemplo.c
+++ exemplo.c
@@ -5,7 +5,7 @@ int main() {
   printf ("mackenzie - FCI\n");
   printf ("Ciencia da computacao\n");
   printf ("algoritmos e Programação II\n");
-  
+  printf ("A FCI Mackenzie foi fundada em 1970\n");
 
 
 return 0;
